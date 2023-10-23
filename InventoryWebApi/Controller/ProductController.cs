ad50367207441a108f728c71691dc05da7b4c0b1
Sonita <sonita.borin2@gmail.com>
2023-10-23 15:56:57 +0700

    UpdateController

--- InventoryWebApi/Controller/ProductController.cs
+++ InventoryWebApi/Controller/ProductController.cs
@@ -1,6 +1,7 @@
 
 using InventoryLib.Interface;
 using InventoryLib.Models;
+using InventoryLib.Models.Request.Product;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryWebApi.Controller;
@@ -15,9 +16,66 @@ public class ProductController : ControllerBase
         _service = service;
     }
     [HttpGet]
-    public IActionResult Index()
+    public IActionResult GetAllProducts()
     {
         var products = _service.GetAllProducts();
         return Ok(products);
+    }
+    [HttpGet("getById")]
+    public IActionResult GetById([FromBody] Key key)
+    {
+        try
+        {
+            var product = _service.GetProductById(key.Id);
+            return Ok(product);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }        
+    }
+
+    [HttpPost]
+    public IActionResult CreateProduct([FromBody] ProductCreateReq request)
+    {
+        try
+        {
+            var product = _service.CreateProduct(request);   
+            return Ok("Create successfully");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }
+    }
+    [HttpPut]
+    public IActionResult UpdaeProduct([FromBody] ProductUpdateReq request)
+    {
+        try
+        {
+            var product = _service.UpdateProduct(request);   
+            return Ok("Update successfully");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }
+    }
+    [HttpDelete]
+    public IActionResult DeleteProduct([FromBody] Key request)
+    {
+        try
+        {
+            var product = _service.DeleteProduct(request);   
+            return Ok("Delete successfully");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }
     }
 }
